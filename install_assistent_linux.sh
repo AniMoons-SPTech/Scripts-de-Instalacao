a14a057ebb87950e0d0ce6088a1498cde29fa041
Peterson Maranho Santos <44443488+Voltty@users.noreply.github.com>
2023-11-28 10:26:19 -0300

    install_assistent_linux.sh

---
+++ install_assistent_linux.sh
@@ -0,0 +1,101 @@
+apt-get -y update 
+apt-get -y upgrade 
+PURPLE='0;35'
+NC='\033[0m' 
+VERSAO=11
+	
+echo  "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Olá Aluno, serei seu assistente para instalação do Java!;"
+sleep 2
+echo  "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Verificando aqui se você possui o Java instalado...;"
+sleep 2
+
+java -version
+if [ $? -eq 0 ];then
+	echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) : Olá você já tem o java instalado!!"
+	sleep 2
+else
+	echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Opa! Não identifiquei nenhuma versão do Java instalado, mas sem problemas, irei resolver isso agora!"
+	sleep 2
+	echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Adicionando o repositório!"
+	sleep 2
+	add-apt-repository ppa:webupd8team/java -y
+	clear
+	echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7)  Atualizando Pacotes! Quase lá."
+	sleep 2
+	apt update -y
+	clear
+	echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Preparando para instalar a versão 11 do Java. Confirme a instalação quando solicitado ;D"
+	sleep 1
+	apt install default-jre ; apt install openjdk-11-jre-headless; -y
+	clear
+	echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Java instalado com sucesso!"
+	sleep 1
+fi
+echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Olá Aluno, serei seu assistente para instalação do Python 3, pip e Docker!;"
+sleep 2
+
+# Verificar se o Python 3 está instalado
+python3 --version
+if [ $? -eq 0 ]; then
+    echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) O Python 3 já está instalado!"
+    sleep 2
+else
+    echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Opa! Não identifiquei o Python 3 instalado, mas sem problemas, irei resolver isso agora!"
+    sleep 2
+
+    echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Instalando o Python 3..."
+    apt update -y
+    apt install python3 -y
+    clear
+    echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Python 3 instalado com sucesso!"
+    sleep 1
+fi
+
+# Verificar se o pip está instalado
+pip --version
+if [ $? -eq 0 ]; then
+    echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) O pip já está instalado!"
+    sleep 2
+else
+    echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Opa! Não identifiquei o pip instalado, mas sem problemas, irei resolver isso agora!"
+    sleep 2
+    echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Instalando o pip..."
+    apt install python3-pip -y
+    clear
+    echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) pip instalado com sucesso!"
+    sleep 1
+fi
+
+# Verificar se o Docker está instalado
+docker --version
+if [ $? -eq 0 ]; then
+    echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) O Docker já está instalado!"
+    sleep 2
+else
+    echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Opa! Não identifiquei o Docker instalado, mas sem problemas, irei resolver isso agora!"
+    sleep 2
+    echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Adicionando o repositório do Docker!"
+    apt update -y
+    apt install docker.io -y
+    clear
+    echo "$(tput setaf 10)[Bot assistant]:$(tput setaf 7) Docker instalado com sucesso!"
+    sleep 1
+    
+fi
+
+pip install nvidia-nvml-dev-cu12
+
+apt install wget
+
+docker run -d --name mysql-monitoons --restart always -e MYSQL_ROOT_PASSWORD=animoons -e MYSQL_DATABASE=monitoons -e MYSQL_USER=animoons -e MYSQL_PASSWORD=animoons -p 3306:3306 voltty/mysql-monitoons:0.0.4
+
+
+wget https://github.com/AniMoons-SPTech/Java-Monitoons/blob/0061c408613833b1d3b97eb2409ae310c9226550/JARS/Monitoons-image-docker.jar -O Monitoons.jar
+
+init 6
+
+
+
+
+
+
